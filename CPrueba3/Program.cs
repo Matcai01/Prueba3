add4c3664ec6ff319d72ea4de5940a667efd2948
Matcai01 <matiashernanotazu@gmail.com>
2025-05-07 22:07:11 -0300

    Commiteo un cambio

--- CPrueba3/Program.cs
+++ CPrueba3/Program.cs
@@ -13,6 +13,7 @@ namespace CPrueba3
             Console.WriteLine("Hola mundo");
             Console.WriteLine("Ta complicado git eh");
             Console.WriteLine("Subo un cambio");
+            Console.WriteLine("Prueba de commit 1");
         }
     }
 }
